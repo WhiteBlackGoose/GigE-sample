4b4eace56a2f174fca848fb09310e5f07a8edd9e
WhiteBlackGoose <wbg@angouri.org>
2024-11-20 22:46:39 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ async fn main() {
 
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([1280.0, 960.0]),
+        centered: true,
         ..Default::default()
     };
     eframe::run_native(
@@ -38,14 +39,11 @@ struct MyApp {
 }
 
 impl MyApp {
-    fn rgb2egui(rgb: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> ColorImage {
-        egui::ColorImage::from_rgb([rgb.width() as usize, rgb.height() as usize], rgb)
-    }
     pub fn new(ctx: &egui::Context) -> Self {
         Self {
             handle: ctx.load_texture(
                 "s",
-                Self::rgb2egui(&ImageBuffer::from_vec(1, 1, vec![1, 1, 1]).unwrap()),
+                rgb2egui(&ImageBuffer::from_vec(1, 1, vec![1, 1, 1]).unwrap()),
                 egui::TextureOptions::LINEAR,
             ),
             ip_addr: String::from_str("192.168.1.3").unwrap(),
@@ -56,33 +54,20 @@ impl MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        ctx.set_pixels_per_point(2.0);
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.add(TextEdit::singleline(&mut self.ip_addr));
-            if ui.button("Stream").clicked() && self.cam.is_none() {
-                let mut camera =
-                    cameleon::gige::enumerate_cameras(Ipv4Addr::from_str(&self.ip_addr).unwrap())
-                        .unwrap()
-                        .swap_remove(0);
-                camera.open().unwrap();
-                camera.load_context().unwrap();
-                let mut ctxt = camera.params_ctxt().unwrap();
-
-                ctxt.node("GainAuto")
-                    .unwrap()
-                    .as_enumeration(&ctxt)
-                    .unwrap()
-                    .set_entry_by_symbolic(&mut ctxt, "Continuous")
-                    .unwrap();
-
-                let prx = camera.start_streaming(3).unwrap();
-                self.cam = Some((camera, prx));
-            }
-            if ui.button("Stop streaming").clicked() && self.cam.is_some() {
-                let (mut cam, _) = self.cam.take().unwrap();
-                cam.stop_streaming().unwrap();
-                cam.close().unwrap();
-                self.cam = None;
-            }
+            ui.horizontal(|ui| {
+                ui.add(TextEdit::singleline(&mut self.ip_addr));
+                if ui.button("Start").clicked() && self.cam.is_none() {
+                    self.cam = Some(get_camera(Ipv4Addr::from_str(&self.ip_addr).unwrap()));
+                }
+                if ui.button("Stop").clicked() && self.cam.is_some() {
+                    let (mut cam, _) = self.cam.take().unwrap();
+                    cam.stop_streaming().unwrap();
+                    cam.close().unwrap();
+                    self.cam = None;
+                }
+            });
 
             let txt = egui::load::SizedTexture::from_handle(&self.handle);
             ui.add(egui::Image::from_texture(txt).shrink_to_fit());
@@ -94,15 +79,15 @@ impl eframe::App for MyApp {
             let Ok(buf) = buf else {
                 return;
             };
-            let rgb = cameleon_image_to_rgb(buf);
-            let img = Self::rgb2egui(&rgb);
+            let rgb = cameleon2rgb(buf);
+            let img = rgb2egui(&rgb);
             self.handle.set(img, egui::TextureOptions::LINEAR);
         });
         ctx.request_repaint();
     }
 }
 
-fn cameleon_image_to_rgb(buf: Payload) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+fn cameleon2rgb(buf: Payload) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     let mut raw = Cursor::new(buf.payload());
     let mut rgb = vec![0u8; buf.payload().len() * 3];
     let ii = buf.image_info().unwrap();
@@ -120,4 +105,27 @@ fn cameleon_image_to_rgb(buf: Payload) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     let buffer: ImageBuffer<Rgb<u8>, Vec<u8>> =
         ImageBuffer::from_vec(ii.width as u32, ii.height as u32, rgb).unwrap();
     buffer
+}
+
+fn rgb2egui(rgb: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> ColorImage {
+    egui::ColorImage::from_rgb([rgb.width() as usize, rgb.height() as usize], rgb)
+}
+
+fn get_camera(ip_addr: Ipv4Addr) -> (Camera<ControlHandle, StreamHandle>, PayloadReceiver) {
+    let mut camera = cameleon::gige::enumerate_cameras(ip_addr)
+        .unwrap()
+        .swap_remove(0);
+    camera.open().unwrap();
+    camera.load_context().unwrap();
+    let mut ctxt = camera.params_ctxt().unwrap();
+
+    ctxt.node("GainAuto")
+        .unwrap()
+        .as_enumeration(&ctxt)
+        .unwrap()
+        .set_entry_by_symbolic(&mut ctxt, "Continuous")
+        .unwrap();
+
+    let prx = camera.start_streaming(3).unwrap();
+    (camera, prx)
 }
